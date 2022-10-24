724be31656644601732b8b572b2caf8f2c269ed1
Lucas <lucas_linhares_@hotmail.com>
2022-10-24 13:07:57 -0300

    Buscador de músicas implementado

--- src/pages/Musicas/index.js
+++ src/pages/Musicas/index.js
@@ -11,15 +11,25 @@ function Musicas(){
 
     const usuario = localStorage.getItem('usuarioLogado')
     const [musicas, setMusicas] = useState([]);
+    const [busca, setBusca] = useState('');
+
+    function atualizaBusca(e){
+        let valorDigitado = e.target.value;
+        setBusca(valorDigitado);
+    }
 
 
     useEffect( () => {   // Após o componente ser renderizado -- Set nas musicas com os dados da porta
         axios.get(`http://localhost:3001/musicas`)
             .then((resposta) => setMusicas(resposta.data))
     },[])
- 
 
-    const resultado = musicas?.map( (m) => { 
+
+    const musicasFiltradas = musicas
+        .filter((musica) => musica.nome.toLowerCase().includes(busca.toLowerCase()));
+        
+        
+    const resultado = musicasFiltradas?.map( (m) => { 
         return(
             <div>
                 <li className='musica'>
@@ -32,11 +42,13 @@ function Musicas(){
         )
     })
 
+
     return(
         <div>
             <Header/>
-            <main className='playlist-main'>
+            <main className='musicas-main'>
                 <h2 className='title-musicas'>Musicas</h2>
+                <input type='text' className='buscador' placeholder='Buscar músicas' value={busca} onChange={atualizaBusca}></input>
                 <div className="musicas">
                     <ol>
                         {resultado}
